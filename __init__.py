cdadd13faaec6a226a93a2bb49d40c0467fed736
Andreas Lorensen <andlo@outlook.dk>
2018-10-20 13:36:13 +0200

    ...

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ class PicroftGoogleAiyVoicehat(MycroftSkill):
     
     def initialize(self):
         my_led = LED(channel = 25)
-        my_led.start()
+        #my_led.start()
         my_led.set_state(LED.PULSE_QUICK)
         
         #GPIO.setmode(GPIO.BCM)
