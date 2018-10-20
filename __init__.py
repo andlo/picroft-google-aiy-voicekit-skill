38f704a3fe8510d2ee93a0e7d105accbf1be1d53
Andreas Lorensen <andlo@outlook.dk>
2018-10-20 14:30:09 +0200

    ...

--- __init__.py
+++ __init__.py
@@ -60,7 +60,7 @@ class PicroftGoogleAiyVoicehat(MycroftSkill):
         GPIO.output(25,GPIO.LOW)
 
     def handle_listning(self):  
-        self.emitter.emit(Message("recognizer_loop:record_begin")  
+        self.emitter.emit(Message("recognizer_loop:record_begin"))
 
 
 def create_skill():
