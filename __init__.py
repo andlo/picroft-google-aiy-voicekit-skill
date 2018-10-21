47a3e7f1f9237ed7f491e48e88aab04fbb78b566
Andreas Lorensen <andlo@outlook.dk>
2018-10-21 11:05:58 +0200

    ...

--- __init__.py
+++ __init__.py
@@ -13,20 +13,25 @@ class PicroftGoogleAiyVoicehat(MycroftSkill):
         MycroftSkill.__init__(self)
     
     def initialize(self):
+        try:
+            # pin 23 is the GPIO pin the button is attached to
+            # pin 25 is the GPIO pin the LED light is attached to
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setwarnings(False)
+            GPIO.setup(25,GPIO.OUT)
+            GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            pass
+        except GPIOerror:
+            self.log.warning("Cant initialize GPIO - skill will not load")
+            self.speak_dialog("error.initialise")
+            break
+        finally:    
+            self.schedule_repeating_event(self.handle_button, None,0.1, 'GoogleAIY')
 
-        # pin 23 is the GPIO pin the button is attached to
-        # pin 25 is the GPIO pin the LED light is attached to
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
-        GPIO.setup(25,GPIO.OUT)
-        GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        
-        self.schedule_repeating_event(self.handle_button, None,0.1, 'GoogleAIY')
-
-        self.add_event('recognizer_loop:record_begin',  
-                    self.handle_listener_started)  
-        self.add_event('recognizer_loop:record_end',  
-                    self.handle_listener_ended)
+            self.add_event('recognizer_loop:record_begin',  
+                        self.handle_listener_started)  
+            self.add_event('recognizer_loop:record_end',  
+                        self.handle_listener_ended)
 
     
     def handle_button(self, message):
