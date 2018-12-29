ce6f667e2c621c9dbd2e8a0860fc4e227606ea9d
andlo <andlo@outlook.dk>
2018-12-29 21:23:23 +0000

    added license

--- __init__.py
+++ __init__.py
@@ -1,3 +1,21 @@
+"""
+skill picroft-google-aiy-voicekit
+Copyright (C) 2018  Andreas Lorensen
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from mycroft import MycroftSkill
 from mycroft.messagebus.message import Message
 
